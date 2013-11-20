e49c52e1c20b05a7eb9e513a606be5973683e06c
Eric <ebeecroft@mail.csuchico.edu>
2013-11-19 19:19:01 -0800

    Created Battlefuncs and ruby connections

--- formulas/startbattle.cpp
+++ formulas/startbattle.cpp
@@ -1,7 +1,12 @@
+//Prototypes for functions
+double hitfunc(int speed1, int speed2); //Takes in the speed of two combatents
+double chancefunc(); //Returns a random number 
+int damagefunc(int attack1, int defense2, int level1); //Takes in the attack and level of one combatent and defense of other
+
 int startbattlefunc(int health1, int attack1, int defense1, int speed1, int level1, int &damage1, int health2, int attack2, int defense2, int speed2, int level2, int &damage2) //Takes in all of the two combatents stats
 {
    //Start of Battle
-   if((health1 > 0 && health2 > 0)) //Do this if both hps are greater then Zero
+   if((health1 > 0) && (health2 > 0)) //Do this if both hps are greater then Zero
    {
       double mhit, phit, pchance, mchance = 0;
       //Calculate hit(Always running)
