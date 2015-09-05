464141489305d1001977a8eb40c87f60ed696d67
Eric <ebeecroft@mail.csuchico.edu>
2015-09-05 14:43:37 -0700

    Removed the old version 1 code

--- formulas/datagrabber.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-int datagrabberfunc(char *argv[], int &counter)
-{
-   //Returns the data from an argv to the calling variable
-   int data = 0;
-   data = atoi(argv[counter]);
-   counter++;
-   return data;
-}
